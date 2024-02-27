06ad5ad042571ab6c5bc7f11f9d8496bd6797294
009661@#m <tari.almutairi1995@gmai.com>
2024-02-27 22:51:51 +0300

    edit

---
+++ src/test/java/Homework/Day05/CH.java
@@ -0,0 +1,55 @@
+package Day05;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class CH {
+
+    static WebDriver driver;
+
+    @BeforeClass
+
+    public static void chaleng(){
+        driver = new ChromeDriver();
+        driver.get("https://webdriveruniversity.com/To-Do-List/index.html");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+
+    @Test
+    public void tess1(){
+
+        WebElement add = driver.findElement(By.tagName("input"));add.sendKeys("Go my Hu" + Keys.ENTER);
+        Assert.assertTrue( add.isDisplayed());
+
+    }
+
+    @Test
+    public void test2(){
+      WebElement delete =  driver.findElement(By.xpath("//*[@id=\"container\"]/ul/li[2]/span/i"));
+              delete.click();
+              Assert.assertTrue(delete.isDisplayed());
+
+
+    }
+    @Test
+    public void test3(){
+
+       WebElement todolist=  driver.findElement(By.xpath("//*[@id=\"container\"]/ul/li[1]"));
+        todolist.click();
+        Assert.assertTrue(todolist.isDisplayed());
+
+
+    }
+}
---
+++ src/test/java/Homework/Day05/HW1.java
@@ -0,0 +1,45 @@
+package Day05;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class HW1 {
+//    Task 1:
+//    Go to URL: https://demoqa.com/radio-button
+//    Verify whether all 3 options given to the question can be selected.
+//    When each option is selected, print the following texts on the console.
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void test(){
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void testcheek(){
+        driver.get("https://demoqa.com/radio-button");
+        JavascriptExecutor js = (JavascriptExecutor) driver ; ;
+        js.executeScript("window.scrollTo(0,250)");
+        List<WebElement> buttons = driver.findElements(By.xpath("//input[@type='radio']"));
+
+        for (WebElement button : buttons) {
+            button.click();
+
+            String select = button.getAttribute("value");
+            System.out.println(" the option is  : " + select);
+        }
+
+    }
+
+
+}
